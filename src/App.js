9b2eea33adc653f03e75b326ff027bd603f70b33
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-08-03 17:38:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useState } from 'react';
-import Navbar from './Navbar';
+import Navbar from './Navbar.jsx';
 import Topsection from './Topsection.jsx';
 import MidSection from './MidSection';
 import NewsLetter from './NewsLetter';
+import SignUp from './SignUp.jsx';
+import Login from './Login';
 
 const App = () =>{
+    const [toggle, setToggle] = useState(false);
+    const [clickSignUp, setClickSignUp] = useState(false);
+    
 
     return(
         <>
-           <Navbar />
-           <Topsection />
-           <MidSection />
-           <NewsLetter />
+           {clickSignUp && <SignUp closeSignUp={setClickSignUp} className="z-[2]"/>}
+           <Navbar openToggle={setToggle} className="z-0" />
+           {toggle && <Login closeToggle={setToggle} className="z-[2]"/>}  
+           <Topsection openSignUp={setClickSignUp} className="z-0" />
+           <MidSection openSignUp={setClickSignUp} className="z-0" />
+           <NewsLetter className="z-0" />
         </>
         
     )
