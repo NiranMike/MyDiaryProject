83a7fdca919ba611c8730233bc5eb9de91446a63
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2022-08-08 22:59:10 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    screens: {
+     'xsm':'320px',
+   // => @media (min-width: 320px) { ... }
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {},
   },
   plugins: [],
